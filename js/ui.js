a9d07f5f424eadc2720ae4a4662a0680100a05fb
aestforsoul <lishchinskayaa@ukr.net>
2022-06-24 23:47:29 +0300

    changes

--- js/ui.js
+++ js/ui.js
@@ -1,32 +1,35 @@
 export default class UI {
   static printProducts(data) {
-
     const content = document.querySelector(".info");
-    content.innerHTML = "";
-    
-    const product = document.createElement('div');
-    product.classList.add('info__product');
 
-    const productTitle = document.createElement('div');
-    productTitle.classList.add('product__title');
+    const product = document.createElement("div");
+    product.classList.add("info__product");
+
+    const productTitle = document.createElement("div");
+    productTitle.classList.add("product__title");
     productTitle.textContent = `${data.title}`;
     product.appendChild(productTitle);
 
-    const productCategory = document.createElement('p');
-    productCategory.classList.add('product__info');
+    const productCategory = document.createElement("p");
+    productCategory.classList.add("product__info");
     productCategory.textContent = `Category: ${data.category}`;
     product.appendChild(productCategory);
 
-    const productBrand = document.createElement('p');
-    productBrand.classList.add('product__info');
+    const productBrand = document.createElement("p");
+    productBrand.classList.add("product__info");
     productBrand.textContent = `Brand: ${data.brand}`;
     product.appendChild(productBrand);
 
-    const productDesc = document.createElement('p');
-    productDesc.classList.add('product__info');
+    const productDesc = document.createElement("p");
+    productDesc.classList.add("product__info");
     productDesc.textContent = `Description: ${data.description}`;
     product.appendChild(productDesc);
 
     content.appendChild(product);
-    }
+  }
+
+  static clearList() {
+    const content = document.querySelector(".info");
+    content.innerHTML = "";
+  }
 }
--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-import UI from './js/ui.js';
-import Request from './js/request.js';
-
+import UI from "./js/ui.js";
+import Request from "./js/request.js";
 
 const oneBtn = document.querySelector(".buttons__one-btn");
 const allBtn = document.querySelector(".buttons__all-btn");
 const inputValue = document.querySelector(".chr-input");
 
-oneBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      const id = inputValue.value.trim();
-      if(id === '') {
-        alert('Please, enter id');
-        return;
-      } else if(id > 100) {
-        alert(`There is no product with id - ${id}`);
-        return;
-      }
-
-      let product = Request.getOne(id);
-      UI.printProducts(product);
-      inputValue.value = '';
+oneBtn.addEventListener("click", (event) => {
+  try {
+    event.preventDefault();
+    const id = inputValue.value.trim();
+    if (id === "") {
+      alert("Please, enter id");
+      inputValue.value = "";
+      return;
+    } else if (id > 100 || id <= 0) {
+      alert(`There is no product with id - ${id}`);
+      inputValue.value = "";
+      return;
+    }
+    UI.clearList();
+    Request.getOne(id).then((data) => UI.printProducts(data));
+    inputValue.value = "";
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-allBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      let products = Request.getAll();
-      products.map(product => UI.printProducts(product));
+allBtn.addEventListener("click", (event) => {
+  try {
+    event.preventDefault();
+    UI.clearList();
+    Request.getAll().then((data) =>
+      data.products.forEach((element) => UI.printProducts(element))
+    );
+  } catch (error) {
+    console.error(error);
+  }
 });
